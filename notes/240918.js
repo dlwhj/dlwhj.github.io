36e954075e12540af4f002e6e64e4f982f4046ab
dlwhj <129026439+dlwhj@users.noreply.github.com>
2024-09-18 14:26:51 +0900

    Update 240918.js

--- notes/240918.js
+++ notes/240918.js
@@ -1,35 +1,183 @@
-const problems = [
     {
-        prompt: "M39 GUN에 사용되는 20MM 탄약의 Link Type은?",
-        options: ["M14", "M12", "M11", "M10"],
+        prompt: "20MM 탄약의 구성품으로 맞지 않는 것은?",
+        options: ["cartridge case", "projectile", "primer", "gun muzzle"],
+        answer: 4,
+    },
+    {
+        prompt: "20MM 탄약 중 TPT의 색채 표기는?",
+        options: ["백색 탄체에 청색 표기", "청색 탄체에 백색 표기", "국방색 탄체에 청색 표기", "황색 탄체에 백색 표기"],
         answer: 2,
     },
     {
-        prompt: "MK-82 GP BOMB에 MAU-93 Conical Fin 장착 시 사용되는 Shaft Type은?",
-        options: ["MAU-86/B-5", "MAU-86/B-6", "MAU-86/B-7", "MAU-86/B-8"],
+        prompt: "MK-82 GP BOMB의 내부 충진물이 Tritonal일 때 탄체 전방 황색띠의 폭은 얼마인가?",
+        options: ["1\"", "2\"", "3\"", "4\""],
+        answer: 3,
+    },
+    {
+        prompt: "M61 GUN 계열에 사용되는 Link Type은?",
+        options: ["M14", "M12", "M11", "M10"],
         answer: 1,
     },
+    {
+        prompt: "20MM HEI 탄약의 위험 등급은?",
+        options: ["1.1", "1.2", "1.3", "1.4"],
+        answer: 2,
+    },
+    {
+        prompt: "이동형 탄약 조립 장비 RAMS 설치 및 운용 시 설치 장쇼의 크기로 알맞은 것은?",
+        options: ["20X100FT", "50X100FT", "20X200FT", "50X200FT"],
+        answer: 2,
+    },
+    {
+        prompt: "BDU-33의 외부 포장상자가 손상되었을 때 조치사항으로 잘못 나열된 것은?",
+        options: ["손상된 부분 제거", "교체에 의한 수리", "품목 폐기", "사용가능한 상자에 재포장"],
+        answer: 3,
+    }, {
+        prompt: "CXU-3 SIGNAL의 정기검사 주기는 몇년인가?",
+        options: ["5년", "6년", "7년", "8년"],
+        answer: 1,
+    }, {
+        prompt: "BDU-33의 외부 포장상자는 내용물이 과도하게 느슨할 경우 어떤 조치를 하여야 하는가?",
+        options: ["내용물 추가 저장후 포장", "적절한 포장재료로 내용물을 고정", "밴딩으로 강하게 묶음", "폐기처리 한다"],
+        answer: 2,
+    }, {
+        prompt: "MK-82 GP BOMB의 순폭약량은?",
+        options: ["496LBS", "492LBS", "192LBS", "1196LBS"],
+        answer: 3,
+    }, {
+        prompt: "항공기용 기총과 20MM 탄약에 사용되는 LINK가 서로 맞게 연결된 것은?",
+        options: ["M39 GUN - M12 Link", "M61 GUN - M12 Link", "M39 GUN - M12, M14 Link", "M61 GUN - M12, M14 Link"],
+        answer: 1,
+    }, {
+        prompt: "20MM M56 HEI 탄약의 색채 표기는?",
+        options: ["청색 탄체에 백색 표기", "흑색 탄체에 황색 표기", "황색 탄체에 흑색 표기", "국방색 탄체에 흑색 표기"],
+        answer: 3,
+    }, {
+        prompt: "MK-82 GP BOMB에 사용되는 Conical Fin Type은?",
+        options: ["M131", "MAU-93", "BSU-49", "MAU-94"],
+        answer: 2,
+    }, {
+        prompt: "ATU-35 Drive Assembly 장착 시 TAB의 굽힘 정도는?",
+        options: ["120도 이상", "90도 이상", "60도 이상", "45도 이상"],
+        answer: 4,
+    }, {
+        prompt: "MK- 82 GP BOMB의 주기검사 주기는?",
+        options: ["3년", "5년", "7년", "10년"],
+        answer: 4,
+    }, {
+        prompt: "MK-82 GP BOMB 무게의 몇 %가 순폭약량의 무게인가?",
+        options: ["56", "46", "36", "26"],
+        answer: 3,
+    }, {
+        prompt: "MK-82 GP BOMB의 전\후방 Adapter Booster Type는?",
+        options: ["M148, M147", "M904, M905", "M904, FMU-54", "FMU-26, FMU-54"],
+        answer: 1,
+    }, {
+        prompt: "MK-82 BOMB의 후방 신관의 화재 기호 및 혼합저장 그룹은?",
+        options: ["1.1D", "1.1E", "1.2D", "1.2E"],
+        answer: 3,
+    }, {
+        prompt: "GP BOMB에서 SUSPENSION LUG, BOOSTER, FIN을 장착하는 곳은?",
+        options: ["ARMING WIRE", "FUZE", "BOMB BODY", "WING"],
+        answer: 3,
+    }, {
+        prompt: "GP BOMB 구성품 중 HIGH DRAG TYPE, LOW DRAG TYPE으로 투하 후 비행자세 유지하는 것은?",
+        options: ["FIN", "FUZE", "BOMB BODY", "ARMING WIRE"],
+        answer: 1,
+    }, {
+        prompt: "20MM T.P에 사용되는 CARTRIDGE 계열은 무엇인가?",
+        options: ["M55계열", "M220계열", "M53계열", "M56계열"],
+        answer: 1,
+    }, {
+        prompt: "M904 FUZE의 사용되는 M9 DELAY 몇 가지로 분류되는가?",
+        options: ["4", "6", "8", "10"],
+        answer: 2,
+    }, {
+        prompt: "GP BOMB에 NOSE YELLOW BAND가 1 INCH일 때 MAIN CHARGE는?",
+        options: ["COMP' B", "T.N.T", "TRITONAL", "M9"],
+        answer: 2,
+    }, {
+        prompt: "GP BOMB에 NOSE YELLOW BAND가 2 INCH일 때 MAIN CHARGE는?",
+        options: ["T.N.T", "TRITONAL", "MB", "COMP' B"],
+        answer: 4,
+    }, {
+        prompt: "GP BOMB에 NOSE YELLOW BAND가 3 INCH일 때 MAIN CHARGE는?",
+        options: ["TRITONAL", "COMP' B", "M9", "T.N.T"],
+        answer: 1,
+    }, {
+        prompt: "MK-82 DUMMY BOMB의 주요 충진물은?",
+        options: ["가스", "물", "기름", "시멘트"],
+        answer: 4,
+    }, {
+        prompt: "투하 및 발사 방식에 의한 MK-82 GP BOMB의 분류는?",
+        options: ["공대공용", "공대지용", "지대지용", "지대공용"],
+        answer: 2,
+    }, {
+        prompt: "MK-82 GP BOMB의 Nose Adapter Booster TYPE은?",
+        options: ["M148", "M147", "M339", "M129"],
+        answer: 1,
+    }, {
+        prompt: "MK-82 GP BOMB의 Tail Adapter Booster TYPE은?",
+        options: ["M148", "M147", "M339", "M129"],
+        answer: 2,
+    }, {
+        prompt: "MK-82 탄체검사중 FUZE WELL의 나사산이 몇개 이상 손상 되었을 때 폭탄을 폐기하는가?",
+        options: ["1개", "2개", "3개", "나사산 손상이 있더라도 정상조립이 가능하고 유격이 없다면 상관없다"],
+        answer: 3,
+    }, {
+        prompt: "IMPULSE CARTRIDGE 검사종류 중 불필요한 검사는 무엇인가?",
+        options: ["SMI", "PI", "SI", "RI"],
+        answer: 1,
+    }, {
+        prompt: "MK-82 신관 취급시 개봉전/개봉후에 따라 저장 수명이 달라지는데 FUZE, BOMB, NOSE M905 SERIES의 저장 수명으로 알맞은 것은?",
+        options: ["30년/INDEF", "47년/INDEF", "70년/INDEF", "80년/INDEF"],
+        answer: 2,
+    }, {
+        prompt: "다음은 IMPULSE CARTRIDGE 불출시 우선불출될 품목들이다. 적합하지 않은것은 무엇인가?",
+        options: ["가장 오래된 품목", "가장 최신 품목", "LITE 품목", "SEAL 파손"],
+        answer: 2,
+    }, {
+        prompt: "KCCU-44/B CARTRIDGE의 주기검사 주기가 맞는것은?",
+        options: ["1년", "3년", "5년", "7년"],
+        answer: 3,
+    }, {
+        prompt: "MK-82 탄체검사중 LUG WELL의 나사산이 몇% 이상 손상 되었을 때 폭탄을 폐기하는가?",
+        options: ["5% 이상", "10%이상", "20%이상", "30%이상"],
+        answer: 4,
+    }, {
+        prompt: "MK-82 신관 취급시 개봉전/개봉후에 따라 저장 수명이 달라지는데 FUZE, BOMB, NOSE M904 SERIES의 저장 수명으로 알맞은 것은?",
+        options: ["50년/INDEF", "70년/INDEF", "80년/INDEF", "100년/INDEF"],
+        answer: 1,
+    }, {
+        prompt: "MK-82 탄체검사중 중대 부식, 뭉쳐진 녹 또는 패임이 폭탄본체의 몇% 이상 발견되었을 때 도장작업을 수행 해야 하는가?",
+        options: ["10% 이상", "20% 이상", "30% 이상", "50% 이상"],
+        answer: 4,
+    }
 ]
 
-const quizEl = document.querySelector("#quiz");
-const quizPromptInputEl = document.querySelector("#quiz-prompt-input");
-const quizPromptOutputEl = document.querySelector("#quiz-prompt-output");
-const quizAnswerInputEl = document.querySelector("#quiz-answer-input");
-const quizAnswerOutputEl = document.querySelector("#quiz-answer-output");
+const quizEl = document.forms["quiz"];
+const quizIndexEl = quizEl.elements["quiz-index"];
+const quizProblemEl = quizEl.elements["quiz-problem"];
 
-function updateOutput() {
-    const promptInput = quizPromptInputEl.value - 1;
-    const answerInput = quizAnswerInputEl.value;
-    const prompt = problems[promptInput].prompt;
-    const options = problems[promptInput].options;
-    const answer = problems[promptInput].answer;
+const updateQuiz = () => {
+    const index = quizIndexEl.value - 1;
+    const prompt = problems[index].prompt;
+    const options = problems[index].options;
+    const answer = problems[index].answer;
 
-    const p = document.createElement("p");
-    p.textContent = prompt;
-    const ol = document.createElement("ol");
-    quizPromptOutputEl.replaceChildren(p, ol);
-    quizAnswerOutputEl.value = (answerInput == answer) ? "Correct" : "Incorrect";
+    const promptP = document.createElement("p");
+    promptP.innerText = "PROMPT: " + prompt;
+    const optionsOl = document.createElement("ol");
+    options.forEach(option => {
+        const li = document.createElement("li");
+        li.innerText = option;
+        optionsOl.appendChild(li);
+    })
+    const answerP = document.createElement("p");
+    answerP.innerText = "ANSWER: " + answer;
+
+    quizProblemEl.replaceChildren(promptP, optionsOl, answerP);
 }
 
-quizEl.addEventListener("input", updateOutput);
-updateOutput();
+quizEl.addEventListener("input", updateQuiz);
+updateQuiz();
